d3bceba1bd33d431d8d706d874c0cb97923121b3
Riho0511 <riho.pinehill@gmail.com>
2023-05-08 14:08:34 +0900

    ドキュメントルートをhtml直下からpublicフォルダに変更

---
+++ html/public/index.php
@@ -0,0 +1,20 @@
+<?php include('../app/pdo.php'); ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>PHP勉強</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <!-- ヘッダー -->
+    <?php include('../resources/layouts/header/index.php'); ?>
+
+    <!-- コンテンツ -->
+    <?php include('../resources/page/login.php'); ?>
+
+    <!-- フッター -->
+    <?php include('../resources/layouts/footer/footer.php'); ?>
+</body>
+</html>
