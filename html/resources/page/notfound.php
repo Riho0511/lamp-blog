b55909de353196fe5028d9dc51893e80f5e5a4a3
Riho0511 <riho.pinehill@gmail.com>
2023-05-12 16:38:04 +0900

    NotFoundページ作成

---
+++ html/resources/page/notfound.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>PHP勉強</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../../../css/style.css">
+</head>
+<body>
+    <div class="contents">
+        <h1>NOT FOUND</h1>
+        <p>ページが見つかりません。</p>
+        <a href="/login">戻る</a>
+    </div>
+</body>
+</html>
